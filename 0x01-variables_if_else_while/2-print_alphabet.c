4076ab12bed92a3cd1386d16fc63ddfc5022f9de
For-bae <samhannah2208@gmail.com>
2023-02-17 21:21:00 -0800

    printing alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+        char *message;
+        int n;
+
+        message ="abcdefghijklmnopqrstuvwxzy";
+        n = 0;
+
+        while (n < 25)
+        {
+                putchar(message[n]);
+                n++;
+        }
+        return (0);
+}
