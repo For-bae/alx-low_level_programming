13537aa12df1cfc83a96c16bdd46981725532152
For-bae <samhannah2208@gmail.com>
2023-02-17 22:23:11 -0800

    added 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char *message;
+	int n;
+
+	message = "abcdefghijklmnopqrstuvwxyz";
+	n = 25;
+
+	while (n >=0 )
+	{
+		putchar(message[n]);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
