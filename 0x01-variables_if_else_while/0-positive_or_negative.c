bf7ae5f41454df71c8832fb3977a5b33595c006a
For-bae <samhannah2208@gmail.com>
2023-02-17 20:04:16 -0800

    fix documentation

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,10 @@
 #include <time.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - Entry
+ * Return: 0 (Success)
+ */
 int main(void)
 {
 	int n;
