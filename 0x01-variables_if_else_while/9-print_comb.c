ddcec3ce51953909670630b54cd84d4836e883f3
For-bae <samhannah2208@gmail.com>
2023-02-17 22:38:03 -0800

    added 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char s10 = "0123456789";
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar(s10[n]);
+		if(n < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
