25d7d8c45c8646ea85da5fbba028798b34445540
For-bae <samhannah2208@gmail.com>
2023-02-17 22:16:02 -0800

    fixed

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -28,14 +28,13 @@ int main(void)
 		message = "and is greater than 5";
 	}
 	else if (y == 0)
-	{	
+	{
 		message = "and is 0";
 	}
-	else if (y < 6 && y != 0) 
+	else if (y < 6 && y != 0)
 	{
 		message = "and is less than 6 and not 0";
 	}
-	
-        printf("Last digit of %s is %d %s\n", str, y, message);
-        return (0);
+	printf("Last digit of %s is %d %s\n", str, y, message);
+	return (0);
 }
