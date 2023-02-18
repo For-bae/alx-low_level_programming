ed2a2cecc96eb80c3113fbc20dc119210442bd7b
For-bae <samhannah2208@gmail.com>
2023-02-17 21:34:12 -0800

    push 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char *message;
+	int n;
+
+	message ="abcdfghijklmnoprstuvwxyz\n";
+	n = 0;
+
+	while (n <= 26)
+	{
+		putchar(message[n]);
+		n++;
+	}
+	return (0);
+}
