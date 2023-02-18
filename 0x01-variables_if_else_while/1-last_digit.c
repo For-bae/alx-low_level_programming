61d5eb7923c1a7e124848e6c15cc2e314b936fb0
For-bae <samhannah2208@gmail.com>
2023-02-17 21:02:46 -0800

    fixed formatting

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,42 +9,46 @@
  */
 int main(void)
 {
-    int n;
-    char str[1000];
-    int lastIndex;
-    int y;
-    char* message;
+	int n;
+        char str[1000];
+        int lastIndex;
+        int y;
+        char *message;
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
     
-    snprintf( str, 1000, "%d", n);
-    lastIndex = strlen(str) - 1;
-    y = atoi(&str[lastIndex]);
+	snprintf( str, 1000, "%d", n);
     
-    if (y > 5)
-    {
-        message = "and is greater than 5";
-    }
-    else if (y == 0)
-    {
-        message = "and is 0";
-    }
-    else if (y < 6 && y != 0)
-    {
-        message = "and is less than 6 and not 0";
-    }
-    else
-    {
-        message = "";
-    }
+	lastIndex = strlen(str) - 1;
+    
+	y = atoi(&str[lastIndex]);
+    
+    
+	if (y > 5)
+	{
+		message = "and is greater than 5";
+	}
+	else if (y == 0)
+	{
+		message = "and is 0";
+	}
+	else if (y < 6 && y != 0)
+	{
+		message = "and is less than 6 and not 0";
+	}
+	else
+	{
+		message = "";
+	}
 
-    if (n < 0)
-    {
-        y = y * -1;
-    }
+    
+	if (n < 0)
+	{
+		y = y * -1;    
+	}
 
-    printf("Last digit of %s is %d %s\n", str, y, message);
-
-    return (0);
+	printf("Last digit of %s is %d %s\n", str, y, message);
+    
+	return (0);
 }
