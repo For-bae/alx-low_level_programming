3081f688bad83fdbf629f4b12383418d28e0b7c2
For-bae <samhannah2208@gmail.com>
2023-02-17 20:34:56 -0800

    remove uneeded

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,11 +14,10 @@ int main(void)
     srand(time(0));
     n = rand() - RAND_MAX / 2;
 
-    int length = snprintf( NULL, 0, "%d", n);
-    char str[1000];
-    snprintf( str, length + 1, "%d", n);
+    char str[100];
+    snprintf( str, 100, "%d", n);
 
-    const char lastDigit = str[strlen(str) - 1];
+    char lastDigit = str[strlen(str) - 1];
     int y = atoi(&lastDigit);
     char* message;
     if (y > 5)
