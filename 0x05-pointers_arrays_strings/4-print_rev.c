cd2aea3cdab46fe678352353c309ec19f308efae
For-bae <samhannah2208@gmail.com>
2023-02-27 17:56:08 -0800

    fixing reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,5 +10,16 @@
 
 void print_rev(char *s)
 {
-	puts (strrev (s));
+	int i, len, temp; 
+	
+	len = strlen(str1);
+	
+	for (i = 0; i < len/2; i++)
+	{
+		temp = s[i];
+		s[i] = str1[len - i - 1];
+		s[len - i - 1] = temp;	
+	}
+
+	printf("%s\n", s);
 }
