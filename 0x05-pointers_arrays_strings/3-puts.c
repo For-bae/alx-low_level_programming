29aa4df0279c2feeb1eb9978e3e084d200c68960
For-bae <samhannah2208@gmail.com>
2023-02-27 17:38:10 -0800

    added puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include <string.h>
+
+/**
+ * _puts - prints the string plus a new line
+ * @s: The string to print
+ *
+ * Return: Length of string
+ */
+
+void _puts(char *s)
+{
+	puts(s);
+	puts("\n");
+}
