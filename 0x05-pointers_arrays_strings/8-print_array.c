c72a2e73c3912f955db309715262b0ec97899bac
For-bae <samhannah2208@gmail.com>
2023-02-28 02:34:19 -0800

    added print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_array - prints formatted elements of integer array
+ * @a: The array
+ * @n: The length of array
+ *
+ * Return: string
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		if (i == n -1)
+		{
+			printf("%d", a[i]);
+		}
+		else
+		{
+			printf("%d, ", a[i]);
+		}
+		i++;
+	}
+	printf("\n");
+}
